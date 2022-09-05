6908b7d185e3d0c2714f6db078365f69687f56be
Berat Çimen <beratcmn@gmail.com>
2022-09-05 16:34:11 +0300

    Simple enemy ai

---
+++ Opponent/Opponent.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var speed: int = 500
+var ball: KinematicBody2D
+
+func _ready() -> void:
+	ball = $"%Ball"
+
+func _process(delta: float) -> void:
+	print(GetOpponentDirection())
+
+func _physics_process(delta: float) -> void:
+	move_and_slide(Vector2(0, GetOpponentDirection() * speed))
+	
+func GetOpponentDirection() -> int:
+	if abs(ball.position.y - position.y) < 25:
+		return 0
+	if ball.position.y < position.y: # ball is lower
+		return -1
+	elif ball.position.y > position.y: # ball is lower
+		return 1
+	else:
+		return 1-0
