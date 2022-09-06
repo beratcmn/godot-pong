6dc0e8c239983251f756ff3d15e8bc54af6cd868
Berat Çimen <beratcmn@gmail.com>
2022-09-06 14:53:03 +0300

    Added reset function and label

--- Level/GameManager.gd
+++ Level/GameManager.gd
@@ -9,6 +9,11 @@ func ResetBall():
 
 func _process(delta: float) -> void:
 	$"%Score".text = str(PlayerScore) + " : " + str(OpponentScore)
+	
+	if Input.is_action_pressed("reset_game"):
+		PlayerScore = 0
+		OpponentScore = 0
+		ResetBall()
 
 func _on_BallBoundary_Left_body_entered(body: Node) -> void:
 	ResetBall()
